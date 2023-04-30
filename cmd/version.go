f369de06ed48a2a3a4bdd4e8ec209ab852186a07
Oleksii <hereisalexius@gmail.com>
2023-04-30 18:36:01 +0000

    Enchance app version output

--- cmd/version.go
+++ cmd/version.go
@@ -10,6 +10,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var appVersion = "1.0.0"
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
@@ -21,7 +22,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("version called")
+		fmt.Println(appVersion)
 	},
 }
 
